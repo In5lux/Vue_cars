b33263e6518823e84bcb0b8afb5376e58164fcd7
In5lux <nikos_almeryda@mail.ru>
2022-01-25 14:52:40 +0300

    Order log

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const car = (name, model, owner, year, phone, image) => ({ name, model, owner, year, phone, image });
+const log = (text, type, date = new Date()) => ({ text, type, date });
 
 const cars = [
 	car('Ford', 'Focus', 'Max', 2016, '+7 929 123 45 67', 'images/focus.jpg'),
@@ -11,15 +12,24 @@ new Vue({
 	data: {
 		cars: cars,
 		car: cars[0],
+		logs: [],
 		selectedCarIndex: 0,
 		phoneVisibility: false,
 		search: '',
 		modalVisibility: false
 	},
 	methods: {
-		selectCar: function (index) {
+		selectCar(index) {
 			this.car = cars[index];
 			this.selectedCarIndex = index
+		},
+		newOrder() {
+			this.modalVisibility = false;
+			this.logs.push(log(`Success order: ${this.car.name} - ${this.car.model}`, 'ok'));
+		},
+		cancelOrder() {
+			this.modalVisibility = false;
+			this.logs.push(log(`Canceled order: ${this.car.name} - ${this.car.model}`, 'cnl'));
 		}
 	},
 	computed: {
@@ -30,6 +40,11 @@ new Vue({
 			return this.cars.filter(car => {
 				return car.name.indexOf(this.search) > -1 || car.model.indexOf(this.search) > -1
 			})
+		}
+	},
+	filters: {
+		date(value) {
+			return value.toLocaleString()
 		}
 	}
 });
